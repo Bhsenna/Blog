00aab7005ba03a0b9d49f8471c8d370291ecf83e
anamaiberg <108688584+anamaiberg@users.noreply.github.com>
2022-09-09 15:31:57 -0300

    Autores e Categorias

--- geral/admin.py
+++ geral/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Categoria, Pagina
+from .models import Categoria, Pagina, Autor
 # Register your models here.
 
 admin.site.register(Categoria)
+admin.site.register(Autor)
 admin.site.register(Pagina)
--- geral/models.py
+++ geral/models.py
@@ -11,14 +11,21 @@ class Categoria(models.Model):
         return self.nome
 
 
-class Pagina(models.Model):
-    data = models.DateTimeField(default=timezone.now)
-    conteude = models.TextField()
-    categorias = models.ForeignKey(Categoria, on_delete=models.DO_NOTHING)
-    descricao = models.TextField(blank=True)
-    titulo = models.CharField(max_length=20)
-    imagem = models.CharField(max_length=15)
-    autor = models.CharField(max_length=20)
+class Autor(models.Model):
+    nome = models.CharField(max_length=20)
 
     def __str__(self):
         return self.nome
+
+
+class Pagina(models.Model):
+    data = models.DateTimeField(default=timezone.now)
+    conteudo = models.TextField()
+    categorias = models.ForeignKey(Categoria, on_delete=models.DO_NOTHING)
+    descricao = models.TextField(blank=True)
+    titulo = models.CharField(max_length=40)
+    imagem = models.CharField(max_length=500, blank=True)
+    autor = models.ForeignKey(Autor, on_delete=models.DO_NOTHING)
+
+    def __str__(self):
+        return self.titulo
