b0a14c406aacd2ad1d0fea11b618159aa9242909
anamaiberg <108688584+anamaiberg@users.noreply.github.com>
2022-09-13 14:15:22 -0300

    Exibição de blogs indivíduais

--- geral/urls.py
+++ geral/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
+    path('<int:id_blog>', views.exibir, name='exibir_pagina'),
 ]
--- geral/views.py
+++ geral/views.py
@@ -5,4 +5,11 @@ from .models import Pagina
 # Create your views here.
 def index(request):
     paginas = Pagina.objects.all()
-    return render(request, 'geral/blog.html', {'paginas': paginas})
+    return render(request, 'geral/blog.html',
+                  {'paginas': paginas})
+
+
+def exibir(request, id_blog):
+    blog = get_object_or_404(Pagina, id=id_blog)
+    return render(request, 'geral/exibir_paginas.html',
+                  {'blog': blog})
