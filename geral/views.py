9023a3cb988a07103183f35fe550b2efbcce3d6f
anamaiberg <108688584+anamaiberg@users.noreply.github.com>
2022-09-15 13:57:53 -0300

    Pagina de Cadastro de Novas Paginas

--- geral/views.py
+++ geral/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Pagina
+from .forms import FormularioCadastro
 
 
 # Create your views here.
@@ -13,3 +14,17 @@ def exibir(request, id_blog):
     blog = get_object_or_404(Pagina, id=id_blog)
     return render(request, 'geral/exibir_paginas.html',
                   {'blog': blog})
+
+
+def cadastro(request):
+    if request.method == 'GET':
+        form = FormularioCadastro()
+        return render(request, 'geral/nova_pagina.html', {'form': form})
+    else:
+        form = FormularioCadastro(request.POST)
+        if form.is_valid():
+            cadastro = form.save()
+            form = FormularioCadastro()
+            return index(request)
+        else:
+            return render(request, 'geral/form.html', {'form': form})
